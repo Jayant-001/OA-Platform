0e60bf8d5380cb9c9330b4f38d1a304daca05aaa
mridulpandey <mridulpandey5277@gmail.com>
2024-12-24 16:04:41 +0530

    Correct get users route

--- apps/server/src/routes/adminRoutes.ts
+++ apps/server/src/routes/adminRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import AdminController from '../controllers/adminController';
+import {UserController} from '../controllers/userController';
 
 const router = Router();
 const adminController = new AdminController();
 
+const userController = new UserController();
+
+router.get('/users', userController.getAllUsers.bind(userController));
+router.get('/users/:userId', userController.getUserById.bind(userController));
 router.get('/', adminController.getAllAdmins.bind(adminController));
 router.get('/:adminId', adminController.getAdminById.bind(adminController));
 router.post('/', adminController.createAdmin.bind(adminController));
--- apps/server/src/routes/userRoutes.ts
+++ apps/server/src/routes/userRoutes.ts
@@ -4,8 +4,6 @@ import { UserController } from '../controllers/userController';
 const router = Router();
 const userController = new UserController();
 
-router.get('/', userController.getAllUsers.bind(userController));
-router.get('/:userId', userController.getUserById.bind(userController));
 router.put('/:userId', userController.updateUser.bind(userController));
 router.delete('/:userId', userController.deleteUser.bind(userController));
 
